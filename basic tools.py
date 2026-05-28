facc0a46fb9b1fa5862795e2255b33c278aab062
RZ <saniazafar193@gmail.com>
2026-05-28 12:53:48 +0500

    Modify print statements to show fewer rows

--- basic tools.py
+++ basic tools.py
@@ -3,8 +3,8 @@ data = {"Name":["Ali","Ahmad","Abbas","Raza","Zafar","Akram"],
         "Subject":["CS","SE","CS","SE","CS","SE"],
         "city":["Karachi","Lahore","Islamabad","Peshawar","Quetta","Faisalabad"]}
 s = pd.DataFrame(data)
-print(s.head(6)) # isse se hum 6 row print ker sektha ha
-print(s.shape) # isse se hum row aur column ki total number print ker sektha ha
-print(s.info()) # isse se hum data ke baare me information milti ha jaise ki data type aur null
-print(s.tail(2)) # isse se hum last 2 row print ker sektha ha
-print(s.describe()) # isse se hum data ke baare me statistical information milti ha
+print(s.head(3)) 
+print(s.shape)
+print(s.info())
+print(s.tail(2))
+print(s.describe()) 
